69352c15656cfc6c3173c32024e06f18e04e0ec3
Dario <dariogabriel113@gmail.com>
2019-04-24 10:57:45 -0300

    Cria HTTPService

---
+++ app/src/main/java/com/dariogabriellsilva/consumindowebservicehttp/services/HTTPService.java
@@ -0,0 +1,51 @@
+package com.dariogabriellsilva.consumindowebservicehttp.services;
+
+import android.os.AsyncTask;
+
+import com.dariogabriellsilva.consumindowebservicehttp.model.CEP;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class HTTPService extends AsyncTask<Void, Void, CEP> {
+    private final String cep;
+
+    public HTTPService(String cep) {
+        this.cep = cep;
+    }
+
+    @Override
+    protected CEP doInBackground(Void... voids) {
+        StringBuilder resposta = new StringBuilder();
+
+        if (this.cep != null && this.cep.length() == 8) {
+            try {
+                URL url = new URL("http://ws.matheuscastiglioni.com.br/ws/cep/find/" + this.cep + "/json/");
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Content-type", "application/json");
+                connection.setRequestProperty("Accept", "application/json");
+                connection.setDoOutput(true);
+                connection.setConnectTimeout(5000);
+                connection.connect();
+
+                Scanner scanner = new Scanner(url.openStream());
+                while(scanner.hasNext()) {
+                    resposta.append(scanner.next());
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
+        }
+
+        return new Gson().fromJson(resposta.toString(), CEP.class);
+    }
+}
